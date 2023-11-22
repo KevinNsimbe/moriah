e7c07e9f4c3e5311b0005dfdabbbfd6977edbeab
Kevin Nsimbe <kevinnsimbe5@gmail.com>
2023-11-22 11:43:44 +0300

    linked html with css

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     <title>Login & Signup Form HTML CSS | CodingNepal</title>
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="css/styles.css" />
     <script src="../custom-scripts.js" defer></script>
   </head>
   <body>
