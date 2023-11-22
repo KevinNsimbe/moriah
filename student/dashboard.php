e41ed8b423ed878d6933a5a36ca17b0a4254db02
Kevin Nsimbe <kevinnsimbe5@gmail.com>
2023-11-22 13:51:28 +0300

    pages loading well in dashboard

--- student/dashboard.php
+++ student/dashboard.php
@@ -30,17 +30,17 @@ $user_name = $_SESSION['user_name'];
         <!-- Navigation bar on the left -->
         <nav>
             <ul>
-                <li><a href="dashboard.php">Dashboard</a></li>
-                <li><a href="academic_info.php">Academic Information</a></li>
-                <li><a href="courses.php">Courses</a></li>
-                <li><a href="results.php">Results</a></li>
-                <li><a href="timetable.php">Timetable</a></li>
-                <li><a href="finance.php">Finance</a></li>
+            <li><a href="dashboard.php" data-page="home.php" class="nav-link">Dashboard</a></li>
+            <li><a href="academic_info.php" data-page="academic_info.php" class="nav-link">Academic Information</a></li>
+            <li><a href="courses.php" data-page="courses.php" class="nav-link">Courses</a></li>
+            <li><a href="results.php" data-page="results.php" class="nav-link">Results</a></li>
+            <li><a href="timetable.php" data-page="timetable.php" class="nav-link">Timetable</a></li>
+            <li><a href="finance.php" data-page="finance.php" class="nav-link">Finance</a></li>
             </ul>
         </nav>
 
         <!-- Dashboard content on the right -->
-        <div class="main-content">
+        <div class="main-content" id="dashboard-content">
             <section>
                 <!-- Your dashboard content -->
                 <p>This is the dashboard. Hello, <?php echo $user_name; ?>!</p>
@@ -51,14 +51,34 @@ $user_name = $_SESSION['user_name'];
 
     <!-- Your JavaScript scripts -->
     <script>
-        // JavaScript to handle the click event on the menu icon
+        // JavaScript to handle the click event on navigation links
         document.addEventListener('DOMContentLoaded', function () {
-            const menuIcon = document.querySelector('.menu-icon');
-            const nav = document.querySelector('nav');
+            const navLinks = document.querySelectorAll('.nav-link');
+            const dashboardContent = document.getElementById('dashboard-content');
 
-            menuIcon.addEventListener('click', function () {
-                nav.classList.toggle('active'); // Toggle the 'active' class on click
+            navLinks.forEach(function (link) {
+                link.addEventListener('click', function (e) {
+                    e.preventDefault(); // Prevent default link behavior
+
+                    const page = this.getAttribute('data-page');
+                    loadPage(page);
+                });
             });
+
+            function loadPage(page) {
+                const xhr = new XMLHttpRequest();
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === XMLHttpRequest.DONE) {
+                        if (xhr.status === 200) {
+                            dashboardContent.innerHTML = xhr.responseText;
+                        } else {
+                            dashboardContent.innerHTML = 'Error loading page.';
+                        }
+                    }
+                };
+                xhr.open('GET', page, true);
+                xhr.send();
+            }
         });
     </script>
 </body>
