f247d059bebd30b3aee229041ec96430b9b6baf3
kevinNsimbe <kevinnsimbe5@gmail.com>
2023-11-26 10:30:30 -0800

    details are now displayed in a from type now planning to apply some css in it

--- student/display_courseform.php
+++ student/display_courseform.php
@@ -1,43 +1,37 @@
-
+<!DOCTYPE html>
+<html>
 <head>
-<link rel="stylesheet" href="css/course_pendingform.css" media="print">
+    <!-- <title>Course Application Details</title> -->
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-<?php
-// Assuming $user_id is available after the user logs in or through session data
-//$pdo = new PDO('mysql:host=localhost;dbname=your_database', 'username', 'password');
 
-$queryCheckPending = "SELECT * FROM course_applications WHERE user_id = ? AND is_confirmed = 0";
-$statementCheckPending = $pdo->prepare($queryCheckPending);
-$statementCheckPending->execute([$user_id]);
+    <?php
+    // Assuming $user_id is available after the user logs in or through session data
+   // $pdo = new PDO('mysql:host=localhost;dbname=your_database', 'username', 'password');
 
-$pendingApplications = $statementCheckPending->fetchAll(PDO::FETCH_ASSOC);
+   $queryCheckPending = "SELECT firstname, lastname, email, phone_number, date_of_birth FROM course_applications WHERE user_id = ? AND is_confirmed = 0";
+   
+   // $queryCheckPending = "SELECT * FROM course_applications WHERE user_id = ? AND is_confirmed = 0";
+    $statementCheckPending = $pdo->prepare($queryCheckPending);
+    $statementCheckPending->execute([$user_id]);
 
-if ($pendingApplications && count($pendingApplications) > 0) {
-    echo "Your application is pending confirmation.<br><br>";
-    
-    // Display details of pending applications for the current user
-   // echo "<h2>Pending Application Details</h2>";
-    echo "<table border='1'>";
-    echo "<tr><th>First Name</th><th>Last Name</th><th>Gender</th><th>Phone Number</th><th>Email</th><th>Date of Birth</th><th>Place of Birth</th><th>Nationality</th><th>Mother Tongue</th><th>ID Number</th><th>Parent Title</th><th>Parent Name</th><th>Parent Address</th><th>Parent Town</th><th>Parent Mobile Number</th><th>Parent Email</th><th>Sponsor</th><th>Course Date</th><th>Intake Date</th><th>English Speaking Level</th><th>Confirmation Status</th></tr>";
-    foreach ($pendingApplications as $application) {
-        echo "<tr>";
-        echo "<td>" . $application['firstname'] . "</td>";
-        echo "<td>" . $application['lastname'] . "</td>";
-        echo "<td>" . $application['Gender'] . "</td>";
-        echo "<td>" . $application['phone_number'] . "</td>";
-        echo "<td>" . $application['email'] . "</td>";
-        // Display other columns similarly...
-        echo "<td>Pending Confirmation</td>";
-        echo "</tr>";
+    $pendingApplication = $statementCheckPending->fetch(PDO::FETCH_ASSOC);
+
+    if ($pendingApplication) {
+        echo "<h2>Your Course Application Details</h2>";
+        echo "<form>";
+        foreach ($pendingApplication as $column => $value) {
+            echo "<label for='$column'>$column:</label><br>";
+            echo "<input type='text' id='$column' name='$column' value='$value' readonly><br><br>";
+        }
+        echo "<input type='submit' value='Submit'>";
+        echo "</form>";
+    } else {
+        echo "Your application is confirmed!";
+        // Or display another message for confirmed applications
     }
-    echo "</table>";
-} else {
-    echo "Your application is confirmed!";
-    // Or display another message for confirmed applications
-}
-?>
-<table class="print-table">
-        <!-- Your PHP-generated table content here -->
-    </table>
+    ?>
+
 </body>
+</html>
